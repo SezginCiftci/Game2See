d7db1f6842c93d3cbbccde38d8f3ce80c16fbd45
Sezgin Çiftçi <88513574+SezginCiftci@users.noreply.github.com>
2022-12-29 09:53:39 +0300

    Initial Commit

---
+++ Game2See/Game2SeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game2SeeApp.swift
+//  Game2See
+//
+//  Created by Sezgin Çiftci on 29.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Game2SeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
